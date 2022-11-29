29f93f76cb22753c681b17a413429058677731b0
John Helder <johnhelderjp@gmail.com>
2022-11-28 21:26:27 -0300

    Adicionando API

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ document.querySelector('.busca').addEventListener('submit', (event) =>{
  
   if(input !== ''){
     mostrarAviso('Carregando..');
+    
+    let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
+    ${encodeURI(input)}&units=metric&lang=pt_br&appid=d06cdb298fafc83c520d5ab677fc477e`);
+    let json = await results.json();
   }
 })
 
