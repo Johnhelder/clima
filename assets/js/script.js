47b9ed9b579027d503bc4e00943031907edbb4d6
John Helder <johnhelderjp@gmail.com>
2022-11-28 21:29:34 -0300

    Adicionando a Key

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.querySelector('.busca').addEventListener('submit', (event) =>{
     mostrarAviso('Carregando..');
     
     let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
-    ${encodeURI(input)}&units=metric&lang=pt_br&appid=d06cdb298fafc83c520d5ab677fc477e`);
+    ${encodeURI(input)}&units=metric&lang=pt_br&appid=3547116e170faf613382252a0c598d08`);
     let json = await results.json();
   }
 })
